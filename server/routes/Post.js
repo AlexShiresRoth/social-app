b49b6b172d75689edba2056816967f93ef90e56f
Alex Rothenberg <alexroth96@gmail.com>
2020-07-26 21:43:26 -0400

    setup post like route, and post delete route

--- server/routes/Post.js
+++ server/routes/Post.js
@@ -20,6 +20,7 @@ router.post('/', auth, [check('text', 'Please add some text to your post').not()
 
 	if (text) newPostFields.text = text;
 	if (status) newPostFields.status = status;
+	newPostFields.user = req.user.id;
 
 	try {
 		const createdPost = new Post(newPostFields);
@@ -33,28 +34,68 @@ router.post('/', auth, [check('text', 'Please add some text to your post').not()
 	}
 });
 
+//@route GET ROUTE
+//@desc GET POSTS
+//@access private
+router.get('/', auth, async (req, res) => {
+	const foundPosts = await Post.find();
+
+	if (!foundPosts) {
+		return res.status(400).json({ msg: 'Hmm something went wrong' });
+	}
+
+	try {
+		res.json(foundPosts);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ msg: 'Internal Server Error' });
+	}
+});
+
 //@route PUT route
 //@desc like post
 //@access private
 
 //TODO test this route
-router.put('/post/:id', auth, async (req, res) => {
+router.put('/like/:id', auth, async (req, res) => {
 	const foundPost = await Post.findById(req.params.id);
 
 	if (!foundPost) {
+		console.error(error);
 		return res.status(400).json({ msg: 'Could not locate post' });
 	}
 
-	if (foundPost.likes.filter((post) => post.user === req.user.id).length > 0) {
-		foundPost.likes.filter((post) => post.user !== req.user.id);
+	if (foundPost.likes.filter((like) => like._id.toString() === req.user.id).length > 0) {
+		console.log(foundPost.likes.filter((like) => like._id.toString() !== req.user.id));
+
+		const removedLikes = foundPost.likes.filter((like) => like._id.toString() !== req.user.id);
+		foundPost.likes = removedLikes;
 		await foundPost.save();
 		return res.json(foundPost);
 	}
 
 	try {
-		foundPost.likes.push(req.user);
+		foundPost.likes.push(req.user.id);
 		await foundPost.save();
 		res.json(foundPost);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ msg: 'Internal Server Errror' });
+	}
+});
+
+//@route DELETE Route
+//@desc Remove Post
+//@access private
+router.delete('/remove/:id', auth, async (req, res) => {
+	const foundPost = await Post.findById(req.params.id);
+
+	if (!foundPost) {
+		return res.status(400).json({ msg: 'Could not locate post' });
+	}
+	try {
+		await foundPost.remove();
+		return res.json({ msg: 'Post Was Removed' });
 	} catch (error) {
 		console.error(error);
 		return res.status(500).json({ msg: 'Internal Server Errror' });
