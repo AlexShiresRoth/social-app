40c1feca6ce2f22c23886194b0874a22ae36614d
Alex Rothenberg <alexroth96@gmail.com>
2020-12-20 21:42:38 -0500

    didnt do alot

--- components/profile/ProfileCreation.js
+++ components/profile/ProfileCreation.js
@@ -1,36 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import style from './ProfileCreation.module.scss';
-import { Image } from 'react-feather';
+import AvatarImport from './profilecreation/AvatarImport';
 const ProfileCreation = (props) => {
 	const [currentIndex, changeIndex] = useState(0);
 	//TODO change this into components
 	const steps = [
 		{
-			caption: 'There are a bunch of things you can add to your profile. None of which are required!',
-			btnText: 'Next',
-			step: '1.  Add an avatar',
-			iconContainer: (
-				<div className={style.missing_img}>
-					<Image size={'3rem'} />
-				</div>
-			),
-			goBack: false,
-			key: 0,
-		},
-		{
-			caption: 'In this step, feel free to write a short bio about yourself',
-			btnText: 'Save',
-			step: '2. Write a short bio',
-			iconContainer: (
-				<div className={style.missing_img}>
-					<Image size={'3rem'} />
-				</div>
-			),
-			goBack: true,
-			key: 1,
+			component: <AvatarImport style={style} changeIndex={changeIndex} key={0} />,
 		},
 	];
+
 	return (
 		<div className={style.profile_onboarding}>
 			{/* create a heading showing step progression */}
@@ -39,39 +19,7 @@ const ProfileCreation = (props) => {
 					<div className={style.onboarding__inner__heading}>
 						<div className={style.progress_bar}></div>
 					</div>
-					<div className={style.onboarding__inner__slides}>
-						{steps
-							.map(({ step, caption, btnText, key, iconContainer = null, goBack }) => {
-								console.log(key, steps.length - 1);
-								return (
-									<div className={style.slide} key={key}>
-										{iconContainer ? iconContainer : null}
-										<p>{caption}</p>
-										<h3>{step}</h3>
-										<div className={style.buttons}>
-											<button
-												className={style.next_btn}
-												onPointerDown={(e) => changeIndex((prevIndex) => prevIndex + 1)}
-											>
-												{btnText}
-											</button>
-											{goBack ? (
-												<button
-													className={style.back_btn}
-													onPointerDown={(e) => changeIndex((prevIndex) => prevIndex - 1)}
-												>
-													Back
-												</button>
-											) : null}
-											{key !== steps.length - 1 ? (
-												<button className={style.skip_btn}>Skip</button>
-											) : null}
-										</div>
-									</div>
-								);
-							})
-							.slice(currentIndex, currentIndex + 1)}
-					</div>
+					<div className={style.onboarding__inner__slides}>{steps[currentIndex].component}</div>
 				</div>
 			</div>
 		</div>
--- components/profile/profilecreation/AvatarImport.js
+++ components/profile/profilecreation/AvatarImport.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'react-feather';
 
-const AvatarImport = (props) => {
-	return <div></div>;
+const AvatarImport = ({ style, changeIndex, key }) => {
+	return (
+		<div className={style.slide} key={key}>
+			<div className={style.missing_img}>
+				<Image size={'3rem'} />
+			</div>
+			<p>There are a bunch of things you can add to your profile. None of which are required!</p>
+			<h3>1. Add an avatar</h3>
+			<div className={style.buttons}>
+				<button className={style.save_btn}>Save</button>
+				<button className={style.next_btn} onPointerDown={(e) => changeIndex((prevIndex) => prevIndex + 1)}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 };
 
 AvatarImport.propTypes = {};
